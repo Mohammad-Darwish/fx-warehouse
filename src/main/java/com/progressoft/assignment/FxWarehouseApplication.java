c2aeb1ad665657cb51392e247dbc7efbd7d43052
Mohammad Darwish <mohammad.darwish@ericsson.com>
2024-02-28 20:04:53 +0100

    first commit

---
+++ src/main/java/com/progressoft/assignment/FxWarehouseApplication.java
@@ -0,0 +1,11 @@
+package com.progressoft.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxWarehouseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FxWarehouseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/progressoft/assignment/FxWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressoft.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
